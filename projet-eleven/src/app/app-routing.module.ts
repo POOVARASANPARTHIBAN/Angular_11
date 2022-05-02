f08037e56a082de13bff016ce52ebd10368a80d3
Poovarasan P <poov3028@global.chainsys.com>
2022-05-02 12:04:48 +0530

    first commit

--- projet-eleven/src/app/app-routing.module.ts
+++ projet-eleven/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+];
+
+@Component({
+
+})
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projet-eleven/src/app/app.module.ts
+++ projet-eleven/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+   
 import { AppComponent } from './app.component';
-
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+// import { LoginComponent } from './login/login.component';
+// import { ForgetPasswordComponent } from './forget-password/forget-password.component';
+// import { NotFoundComponent } from './not-found/not-found.component';
+// import { LogInComponent } from './routing/log-in/log-in.component';
+// import { ForgotPasswordComponent } from './routing/forgot-password/forgot-password.component';
+   
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeFormComponent,
+    // LoginComponent,
+    // ForgetPasswordComponent,
+    // NotFoundComponent,
+    // LogInComponent,
+    // ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
