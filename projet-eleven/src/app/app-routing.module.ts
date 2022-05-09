f852cade0c2d45d2a4f408fc243bff33dd1c3b11
Poovarasan P <poov3028@global.chainsys.com>
2022-05-09 17:37:02 +0530

    first commit

--- projet-eleven/src/app/app-routing.module.ts
+++ projet-eleven/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { AboutComponent } from './about/about.component';
+// import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+// import { NavbarComponent } from './navbar/navbar.component';
 
 
 const routes: Routes = [
-
+  {path: 'Home', component: HomeComponent},
+  {path: 'projects', component: ProjectsComponent}
 ];
-
-@Component({
-
-})
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projet-eleven/src/app/app.module.ts
+++ projet-eleven/src/app/app.module.ts
@@ -1,29 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-   
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeFormComponent } from './employee-form/employee-form.component';
-// import { LoginComponent } from './login/login.component';
-// import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-// import { NotFoundComponent } from './not-found/not-found.component';
-// import { LogInComponent } from './routing/log-in/log-in.component';
-// import { ForgotPasswordComponent } from './routing/forgot-password/forgot-password.component';
-   
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeFormComponent,
-    // LoginComponent,
-    // ForgetPasswordComponent,
-    // NotFoundComponent,
-    // LogInComponent,
-    // ForgotPasswordComponent
+    NavbarComponent,
+    AboutComponent,
+    HomeComponent,
+    ProjectsComponent  
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
